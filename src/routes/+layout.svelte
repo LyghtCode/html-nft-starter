35e7ff020b87cd9aac8f9f7ee0d2d3a250828620
LyghtCode <xikago.musica@gmail.com>
2022-11-18 15:00:40 -0500

    false prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
 		box-sizing: border-box;
 	}
 
-	footer {
+	/* footer {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -46,5 +46,5 @@
 		footer {
 			padding: 12px 0;
 		}
-	}
+	} */
 </style>
