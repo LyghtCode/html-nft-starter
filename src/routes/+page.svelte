5dec4e61d058cdc0a3e8626c8ffe89d3168572fd
LyghtCode <xikago.musica@gmail.com>
2022-11-18 15:57:48 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,16 @@
 	<meta name="description" content="NFT Digital Vinyl" />
 </svelte:head>
 
-<div class="text-column">
+<div class="text-column" style="max-width: 480px;">
 	<h1>LyghtCode's Digital Vinyl NFT</h1>
 
-	<pre style="text-align: center;">
+	<p style="text-align: center;">
 		This small web app exists within an NFT. The idea is to condense an entire album into a single
 		NFT Collectible where the user may also playlist the album inside the NFT. This allows for a few
 		things the first being the entire experience can be enjoyed in the metaverse or virtual NFT
 		Gallery. The second this showcases how interactive NFT's can be. If a webapp can exist within an
 		NFT then what else can be created???? No more monkey jpgs!
-	</pre>
+	</p>
 
 	<p style="text-align: center;">Wᒷ'∷ᒷ ℸ ̣ ⍑ᒷ ᔑꖎ╎ᒷリᓭ, ∴ᒷ ⋮⚍ᓭℸ ̣  ⎓𝙹∷⊣𝙹ℸ ̣.</p>
 
