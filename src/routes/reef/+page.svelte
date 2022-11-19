fd0c10740bd1dc9e6a550534998b71c8d26cbf0c
LyghtCode <xikago.musica@gmail.com>
2022-11-19 17:31:24 -0500

    reef live stream idea

---
+++ src/routes/reef/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	
+
+</script>
+
+<div class="text-column">
+
+	<section id="reef">
+		
+		<iframe autoplay width="540px" height="400px" src="https://www.youtube.com/embed/vhZPwHyDV9Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			
+	</section>
+	
+
+</div>
+
+<style>
+	
+
+	#reef {
+		width: 540px;
+		height: calc(60vh - 48px);
+		padding: 0.7rem 1.5rem 0;
+		box-shadow: 0 0 5px black;
+		background: transparent;
+		color: rgb(27, 157, 233);
+		border-radius: 5px 5px 0 0;
+		/* background: url('https://www.youtube.com/embed/vhZPwHyDV9Q'); */
+	}
+</style>
