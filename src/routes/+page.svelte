ce423e783a42d87d1afbaff16074bf8a14cff8da
LyghtCode <xikago.musica@gmail.com>
2022-12-05 17:20:37 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,33 @@
 	<h1>Life After Life - Bob Peace - Digital Vinyl NFT</h1>
 
 	<p style="text-align: center;">
-		"Hello! My name’s Bob Peace and I want to personally welcome you to the Life After Life digital
-		NFT album. If you own this you either bought it off secondary or collected the single and
-		received this as a free airdrop. Either way, I want to personally thank you and welcome you to
-		being an official Peacekeeper. I write these songs about my own life and as a way to make sense
-		of the world. I hope you enjoy them and keep being positive. With gratitude," Bob Peace
-		For more info visit bobpeace.io.
+		Welcome to the Life After Life Digital Vinyl Album LFG! I’m going to give y’all a few thoughts
+		on this album so you know what the f is going on whilst listening. Firstly, thank you to the
+		Collectors - I’m nothing without y’all so thanks! Y’all are now Peacekeepers in my book so let’s
+		get it :-) Lay With Me - this track I did in Spring 2017 it was after my girl and I split up and
+		then we was getting back together so this is like my lil’ track to her like yo ‘where yo head at
+		what’s the deal?’ Try And Fly High - if I ain’t wrong this was spring 2017 after a terrible
+		roommate situation and heading up to Malibu from SD and then getting back to SD and sitting at
+		the ocean with my dogs thinking like ‘man what am I doing with my life with this music ya know I
+		should be in LA but dang like I chose this so let’s go.’ Broken Child - this was like the first
+		track I wrote for this record it was I think late 2016 I think this was the first joint we did
+		it’s all about being honest and vulnerable like I was a virgin heading off to college and I’m
+		still a paranoid son of a mother lmfao. Enjoy Wildly On The Run - I wrote this down at Sunset
+		Cliffs with a beater ass guitar I had bought at Guitar Center whilst in LA cuz I had to escape
+		my living situation and aint have no time to get my guitar ya feel me? Like my life is nuts.
+		Fall Into The Sky - this the only joint on the record that wasn’t new, it was from 2010. I love
+		this tune cuz I aint find my truth til 2011 so this like ‘tell me how to love God!!!’ Misery
+		Loves Company -this first lyric bout a diff bad roommate but it’s like dang man how come mf love
+		to be miserable and drag all down with them?!? Crazy world. How Many Days? - I love cigarettes.
+		Idfaf. I absolutely love tobacco. I think I only love the ocean more. Anyhow, wrote this late
+		one night in ob like musing ‘how many days on this planet we get?’ We Don’t Know Why - wrote
+		this spring 2017 in Encinitas f guy who lived w me gave me ish about being quiet while I’m
+		writing grammy winning songs LFG!!! Reach The Sky - f love this tune. Jus sweet. Brokenhearted -
+		wrote this on a deck in ob this was the last joint we did in January 2018 when I got back from
+		performing at a conference in hollywood. I was in go mode. Fav track on the record. This record
+		dedicated to my two pups Magic and Molasses who rode w me through all the crazy times when I had
+		to deal w gnarly humans all over this country. As always, all praise is due to God cuz I’d be
+		dead without him. Love to Jesus, as well. ENJOY!!! - peacer
 	</p>
 
 	<h1><a href="/album">Listen to Album >>></a></h1>
