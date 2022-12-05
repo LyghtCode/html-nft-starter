d373b063b92b5c36ba18cddda2f603ef53937d8e
LyghtCode <xikago.musica@gmail.com>
2022-12-05 18:24:51 -0500

    Update audioData.js

--- src/routes/album/audioData.js
+++ src/routes/album/audioData.js
@@ -1,42 +1,42 @@
 export const audioData = [
 	{
 		name: "Lay With Me",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/01%20-%20BobPeace_01v1_LayWithMe.mp3",
 	},
 	{
 		name: "Try & Fly High",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/02%20-%20BobPeace_02v1_TryAndFlyHigh.mp3",
 	},
 	{
 		name: "Broken Child",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/03%20-%20BobPeace_03v1_BrokenChild.mp3",
 	},
 	{
 		name: "Wildly On the Run",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/04%20-%20BobPeace_04v1_WildlyOnTheRun.mp3",
 	},
 	{
 		name: "Fall Into the Sky",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/05%20-%20BobPeace_05v1_FallIntoTheSky.mp3",
 	},
 	{
 		name: "Misery Loves Company",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/06%20-%20BobPeace_06v1_MiseryLovesCompany.mp3",
 	},
 	{
 		name: "How Many Days",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/07%20-%20BobPeace_07v1_HowManyDays.mp3",
 	},
 	{
 		name: "We Don't Know Why",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/08%20-%20BobPeace_08v2_WeDontKnowWhy.mp3",
 	},
 	{
 		name: "Reach the Sky",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/09%20-%20BobPeace_09v1_ReachTheSky.mp3",
 	},
 	{
 		name: "Broken Hearted",
-		url: "https://bafybeickxhhrmerirrnr4oexc67mxh5kmeglbyktmbcphfebyn6igj2dle.ipfs.dweb.link/",
+		url: "https://gateway.pinata.cloud/ipfs/QmTXGBoQti7ApcWE9KG4UC4oi7sZg3Weea8eqKSR9Z2F7b/10%20-%20BobPeace_10v1_BrokenHearted.mp3",
 	}
 ]
