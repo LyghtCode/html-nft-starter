3a8b5ebd0699942c551175119721023f3db232e2
LyghtCode <xikago.musica@gmail.com>
2022-12-05 13:41:43 -0500

    add text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,16 @@
 	<h1>Life After Life - Bob Peace - Digital Vinyl NFT</h1>
 
 	<p style="text-align: center;">
-		This small web app exists within an NFT. The idea is to condense an entire album into a single
-		NFT Collectible where the user may also playlist the album inside the NFT. This allows for a few
-		things the first being the entire experience can be enjoyed in the metaverse or virtual NFT
-		Gallery. The second this showcases how interactive NFT's can be. If a webapp can exist within an
-		NFT then what else can be created???? No more monkey jpgs!
+		Few cool perks we're doing are: 1) After 555 sold buyer can redeem NFT for vinyl "Life After
+		Life" album (buy extra so you have digital and physical!). 2) Via TokenProof hoping to procure
+		some IRL benefits such as discounts at local businesses, etc., simply by presenting and
+		verifying your NFT (see bobpeace.io for list of participating establishments). 3) After 777 sold
+		3% of sales will be donated to Children's Hospital of Los Angeles for the remainder of the 1,111
+		NFTs. When creating this piece, I wanted to highlight the light and color we obtain when
+		overcoming being broken. The love for ourselves we feel once we become our own person. The
+		flowing energy that explodes out of us once we become who we are meant to be. Being broken can
+		always be fixed. For more info please visit bobpeace.io
 	</p>
 
 	<h1><a href="/album">Listen to Album >>></a></h1>
-
-	
-
 </div>
